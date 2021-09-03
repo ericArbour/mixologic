59a6f533a1a038de7149243bae5ab9c675e52f53
Eric Arbour <arboureric@gmail.com>
2021-09-03 16:11:57 -0400

    Save cocktail to cocktail_ingredient relationship

--- apps/api/src/app/cocktails/cocktails.service.ts
+++ apps/api/src/app/cocktails/cocktails.service.ts
@@ -4,36 +4,54 @@ import { Repository } from 'typeorm';
 
 import { CreateCocktailDto } from './dto/create-cocktail.dto';
 import { UpdateCocktailDto } from './dto/update-cocktail.dto';
+import { CocktailIngredient } from './entities/cocktail-ingredient.entity';
 import { Cocktail } from './entities/cocktail.entity';
 
 @Injectable()
 export class CocktailsService {
   constructor(
     @InjectRepository(Cocktail)
-    private repository: Repository<Cocktail>
+    private cocktailRepository: Repository<Cocktail>,
+    @InjectRepository(CocktailIngredient)
+    private cocktailIngredientRepository: Repository<CocktailIngredient>
   ) {}
 
-  create(createCocktailDto: CreateCocktailDto) {
-    const cocktail = this.repository.create(createCocktailDto);
-    return this.repository.save(cocktail);
+  async create(createCocktailDto: CreateCocktailDto) {
+    const cocktail = this.cocktailRepository.create(createCocktailDto);
+    const savedCocktail = await this.cocktailRepository.save(cocktail);
+
+    const promises = createCocktailDto.cocktailIngredients.map(
+      (cocktailIngredientDto) => {
+        const cocktailIngredient = this.cocktailIngredientRepository.create({
+          ...cocktailIngredientDto,
+          cocktailId: savedCocktail.id,
+        });
+
+        return this.cocktailIngredientRepository.save(cocktailIngredient);
+      }
+    );
+    const savedCocktailIngredients = await Promise.all(promises);
+
+    savedCocktail.cocktailIngredients = savedCocktailIngredients;
+    return savedCocktail;
   }
 
   findAll() {
-    return this.repository.find();
+    return this.cocktailRepository.find();
   }
 
   findOne(id: number) {
-    return this.repository.findOne(id);
+    return this.cocktailRepository.findOne(id);
   }
 
   async update(id: number, updateCocktailDto: UpdateCocktailDto) {
     const cocktail = await this.findOne(id);
     if (!cocktail) throw new NotFoundException('Cocktail not found');
 
-    return this.repository.save({ ...cocktail, ...updateCocktailDto });
+    return this.cocktailRepository.save({ ...cocktail, ...updateCocktailDto });
   }
 
   remove(id: number) {
-    return this.repository.softDelete(id);
+    return this.cocktailRepository.softDelete(id);
   }
 }

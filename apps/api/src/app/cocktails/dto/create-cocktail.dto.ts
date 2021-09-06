7b9541420404b31ce0e4a6c93dd81bd2c377da10
Eric Arbour <arboureric@gmail.com>
2021-09-06 19:09:51 -0400

    Add validation for CreateCocktailDto and CreateGlassDto

--- apps/api/src/app/cocktails/dto/create-cocktail.dto.ts
+++ apps/api/src/app/cocktails/dto/create-cocktail.dto.ts
@@ -1,12 +1,49 @@
+import {
+  IsDefined,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsUrl,
+  IsInt,
+  IsNumber,
+  IsPositive,
+  ArrayMinSize,
+} from 'class-validator';
+
 export class CreateCocktailDto {
+  @IsDefined()
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   name!: string;
+
+  @IsDefined()
+  @IsString()
+  @IsUrl()
   url!: string;
+
+  @IsDefined()
+  @IsInt()
+  @IsPositive()
   glassId!: number;
+
+  @ArrayMinSize(2)
   cocktailIngredients!: CreateCocktailIngredientDto[];
 }
 
 export class CreateCocktailIngredientDto {
+  @IsDefined()
+  @IsInt()
+  @IsPositive()
   ingredientId!: number;
+
+  @IsDefined()
+  @IsNumber()
+  @IsPositive()
   amount!: number;
+
+  @IsDefined()
+  @IsInt()
+  @IsPositive()
   unitId!: number;
 }

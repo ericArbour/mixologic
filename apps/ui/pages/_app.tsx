a30a0ba7249b80b25aebb30fcdbc98f102937498
Eric Arbour <arboureric@gmail.com>
2021-09-12 17:15:26 -0400

    Add react-query and setup initial SSR fetching

--- apps/ui/pages/_app.tsx
+++ apps/ui/pages/_app.tsx
@@ -1,23 +1,31 @@
+import { useState } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
         <title>Welcome to ui!</title>
       </Head>
-      <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to ui!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+        <ReactQueryDevtools />
+          <div>
+            <header>
+              <h1>Mixologic</h1>
+            </header>
+            <main>
+              <Component {...pageProps} />
+            </main>
+          </div>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }

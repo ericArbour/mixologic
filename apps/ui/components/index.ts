04171aefe085dee4ca77a5ea322f81c5539bf543
Eric Arbour <arboureric@gmail.com>
2021-09-20 20:33:55 -0400

    Add category form

--- apps/ui/components/index.ts
+++ apps/ui/components/index.ts
@@ -1,2 +1,4 @@
 export * from './header';
+export * from './button';
 export * from './table';
+export * from './text-input';
--- apps/ui/pages/categories/[id].tsx
+++ apps/ui/pages/categories/[id].tsx
@@ -3,8 +3,12 @@ import { QueryClient, useQuery } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
 import { plainToClass } from 'class-transformer';
 import { validateOrReject } from 'class-validator';
+import { useForm } from 'react-hook-form';
 
 import { CategoryDto } from '@mixologic/common';
+
+import { Button } from '../../components';
+import { TextInput } from '../../components';
 
 async function fetchCategory(id: number) {
   const response = await fetch(`http://localhost:4200/api/categories/${id}`);
@@ -41,6 +45,12 @@ const useCategories = (id: number) => {
 };
 
 export default function Category() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
   const { id } = router.query;
 
@@ -48,11 +58,36 @@ export default function Category() {
 
   if (isLoading) return 'Loading...';
 
-  console.log(data);
+  const onSubmit = (data) => console.log(data);
+  console.log(watch('name'));
+  console.log(errors);
 
   return (
     <div className="container flex flex-col mx-auto w-full items-center justify-center">
-      {data.name}
+      <form
+        className="flex w-full max-w-sm space-x-3"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="w-full max-w-2xl px-5 py-10 m-auto mt-10 bg-white rounded-lg shadow dark:bg-gray-800">
+          <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
+            Category
+          </div>
+          <div className="grid max-w-xl grid-cols-2 gap-8 m-auto">
+            <div className="col-span-2">
+              <TextInput
+                label="Name"
+                defaultValue={data.name}
+                {...register('name', { required: true })}
+                required
+                error={errors.name && 'This field is required'}
+              />
+            </div>
+            <div className="col-span-2 text-right">
+              <Button submit={true} label="Save" color="indigo" />
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }

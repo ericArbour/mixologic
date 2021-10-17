32f6b484178e31b45be770223176ccca388d8af0
Eric Arbour <arboureric@gmail.com>
2021-10-17 16:46:35 -0400

    Add create glass page

---
+++ apps/ui/pages/glasses/create.tsx
@@ -0,0 +1,72 @@
+import { useMutation } from 'react-query';
+import { useForm } from 'react-hook-form';
+import { classValidatorResolver } from '@hookform/resolvers/class-validator';
+
+import { CreateGlassDto } from '@mixologic/common';
+
+import { Button, CheckIcon, ErrorIcon, TextInput } from '../../components';
+import { useAnimateLoading } from '../../hooks';
+import { submitMutation } from '../../utils';
+
+const resolver = classValidatorResolver(CreateGlassDto);
+
+export default function CreateGlass() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver });
+
+  const mutation = useMutation<Response, Error, CreateGlassDto>(
+    (createGlassDto) => submitMutation(createGlassDto, 'glasses')
+  );
+  const { shouldAnimateLoading } = useAnimateLoading(mutation);
+
+  const onSubmit = (createGlassDto: CreateGlassDto) =>
+    mutation.mutate(createGlassDto);
+
+  return (
+    <form
+      className="w-full max-w-sm mx-auto px-5 py-10 bg-white rounded-lg shadow dark:bg-gray-800"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
+        Glass
+      </div>
+      <div className="grid max-w-xl grid-cols-2 gap-8 m-auto">
+        <div className="col-span-2">
+          <TextInput
+            label="Name"
+            defaultValue=""
+            {...register('name')}
+            required
+            error={errors.name?.message}
+          />
+        </div>
+        <div className="col-span-2 text-right">
+          <Button
+            submit={true}
+            label="Save"
+            color="indigo"
+            isLoading={mutation.isLoading || shouldAnimateLoading}
+            icon={<CheckIcon className="-ml-1 mr-1" />}
+          />
+        </div>
+        {!shouldAnimateLoading && mutation.isSuccess ? (
+          <div className="col-span-2">
+            <CheckIcon className="-mt-1 mr-1 inline text-green-500" />
+            Glass saved
+          </div>
+        ) : !shouldAnimateLoading && mutation.isError ? (
+          <div className="col-span-2">
+            <p>
+              <ErrorIcon className="mr-1 inline-block -mt-1" />
+              An error occurred:
+            </p>
+            <p>{mutation.error.message}</p>
+          </div>
+        ) : null}
+      </div>
+    </form>
+  );
+}

6aab3c89874ce6d7b5d070a29a8b3f7a8af2fb39
Eric Arbour <arboureric@gmail.com>
2022-02-12 08:37:34 -0500

    Add a passing cypress test

--- apps/ui-e2e/src/integration/app.spec.ts
+++ apps/ui-e2e/src/integration/app.spec.ts
@@ -1,13 +1,9 @@
-import { getGreeting } from '../support/app.po';
+import { getMainHeading } from '../support/app.po';
 
 describe('ui', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to ui!');
+  it('shows the main heading', () => {
+    getMainHeading().contains('Mixologic');
   });
 });

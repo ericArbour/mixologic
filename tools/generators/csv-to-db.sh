3b0463cc527cdb0c85ee74b1a0a5856bee91dbcb
Eric Arbour <arboureric@gmail.com>
2022-02-26 15:37:07 -0500

    Set postgres sequences to max id after insert

--- tools/generators/csv-to-db.sh
+++ tools/generators/csv-to-db.sh
@@ -3,4 +3,5 @@ CONNECTION="postgresql://dev:dev@localhost:5432"
 psql -d $CONNECTION -Atc "select tablename from pg_tables where schemaname='public'" |\
   while read TBL; do
     psql -d $CONNECTION -c "\copy $TBL from '../seed-data/$TBL.csv' delimiter ',' csv header;"
+    psql -d $CONNECTION -c "select setval('${TBL}_id_seq', (select max(id) from $TBL));"
   done
